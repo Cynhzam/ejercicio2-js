f630f6f84d9896909f47c29f9a149d4d30d06842
Cynhzam <cyn.herrerazamudio@gmail.com>
2016-08-25 14:29:36 -0500

    segundo ejercicio js

---
+++ app.js
@@ -0,0 +1,4 @@
+var num1= parseInt(prompt("Ingrese un número"));
+var num2= parseInt(prompt("Ingrese un número"));
+var num3= parseInt(prompt("Ingrese un número"));
+document.write("El resultado es:"+(num1+num2+num3)/3);
